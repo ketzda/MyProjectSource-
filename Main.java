79f4f5c0755bc43261a256292c67a818be64ce6a
ketzda <34332550+ketzda@users.noreply.github.com>
2020-09-09 17:14:49 +0530

    Added Java Project

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.newage.project;
+
+public class Main {
+    public static void main(String[] args) {
+        RangeRover rangeRover = new RangeRover(36);
+        rangeRover.steer(45);
+
+
+    }
+}
